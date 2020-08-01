d0728c7c1bb4f3af7cc86c358b6e33a899300fc4
Rony Herut <ronyheruth@wix.com>
2020-08-01 22:16:34 +0300

    Refactor tests, add call tests

--- test/parser-combinators/language.spec.js
+++ test/parser-combinators/language.spec.js
@@ -1,5 +1,8 @@
 const test = require('ava')
+const { map } = require('ramda')
 const { parse, Builders } = require('../../src/parser-combinators/language')
+
+const [A, B, C] = map(Builders.identifier, 'abc')
 
 test('parses number', t => {
     t.deepEqual(parse('15.6'), Builders.number(15.6))
@@ -10,14 +13,16 @@ test('parses identifiers', t => {
 })
 
 test('parses access', t => {
-    const abAccess = Builders.access(
-        Builders.identifier('a'),
-        Builders.identifier('b')
-    )
+    const abAccess = Builders.access(A, B)
 
     t.deepEqual(parse('a.b'), abAccess)
-    t.deepEqual(
-        parse('a.b.c'),
-        Builders.access(abAccess, Builders.identifier('c'))
-    )
+    t.deepEqual(parse('a.b.c'), Builders.access(abAccess, C))
+})
+
+test.skip('parses calls', t => {
+    const { call, access } = Builders
+    t.deepEqual(parse('a()'), call(A, []))
+    t.deepEqual(parse('a(b, c)'), call(A, [B, C]))
+    t.deepEqual(parse('a()()'), call(call(A, []), []))
+    t.deepEqual(parse('a.b().c()'), call(access(call(access(A, B), []), C), []))
 })
